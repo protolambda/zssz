40431a7ae1d61ffcfa2a28ad4d738a608c3c2ee4
protolambda <proto@protolambda.com>
2019-05-29 16:46:29 +0200

    pointer type to handle go indirection

---
+++ ssz/ssz_ptr.go
@@ -0,0 +1,41 @@
+package ssz
+
+import (
+	"fmt"
+	"reflect"
+	"unsafe"
+)
+
+// proxies SSZ behavior to the SSZ type of the object being pointed to.
+type SSZPtr struct {
+	elemSSZ SSZ
+}
+
+func NewSSZPtr(typ reflect.Type) (*SSZPtr, error) {
+	if typ.Kind() != reflect.Ptr {
+		return nil, fmt.Errorf("typ is not a pointer")
+	}
+	elemSSZ, err := sszFactory(typ.Elem())
+	if err != nil {
+		return nil, err
+	}
+	return &SSZPtr{elemSSZ: elemSSZ}, nil
+}
+
+func (v *SSZPtr) FixedLen() uint32 {
+	return v.elemSSZ.FixedLen()
+}
+
+func (v *SSZPtr) IsFixed() bool {
+	return v.elemSSZ.IsFixed()
+}
+
+func (v *SSZPtr) Encode(eb *sszEncBuf, p unsafe.Pointer) {
+	innerPtr := unsafe.Pointer(*(*uintptr)(p))
+	v.elemSSZ.Encode(eb, innerPtr)
+}
+
+func (v *SSZPtr) Decode(p unsafe.Pointer) {
+	innerPtr := unsafe.Pointer(*(*uintptr)(p))
+	v.elemSSZ.Decode(innerPtr)
+}
