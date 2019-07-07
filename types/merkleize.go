beccc77ef69a20cbeac3dd640d1b9698ff013cd6
protolambda <proto@protolambda.com>
2019-07-07 20:55:01 +0200

    update merkleization, support virtual depth

--- types/merkleize.go
+++ types/merkleize.go
@@ -47,26 +47,37 @@ func GetDepth(v uint32) (out uint8) {
 }
 
 // Merkleize with log(N) space allocation
-func Merkleize(hasher HashFn, count uint32, leaf func(i uint32) []byte) (out [32]byte) {
-	if count == 0 {
+func Merkleize(hasher HashFn, count uint32, limit uint32, leaf func(i uint32) []byte) (out [32]byte) {
+	if count < limit {
+		panic("merkleizing list that is too large, over limit")
+	}
+	if limit == 0 {
 		return
 	}
-	if count == 1 {
+	if limit == 1 {
 		copy(out[:], leaf(0))
 		return
 	}
 	depth := GetDepth(count) + 1
+	limitDepth := GetDepth(limit)
 	tmp := make([][32]byte, depth+1, depth+1)
+
 	j := uint8(0)
 	hArr := [32]byte{}
 	h := hArr[:]
-	for i := uint32(0); i < count; i++ {
-		copy(h[:], leaf(i))
+
+	merge := func(i uint32) {
 		// merge back up from bottom to top, as far as we can
 		for j = 0; ; j++ {
 			// stop merging when we are in the left side of the next combi
 			if i&(uint32(1)<<j) == 0 {
-				break
+				// if we are at the count, we want to merge in zero-hashes for padding
+				if i == count && j < depth {
+					v := hasher.Combi(hArr, ZeroHashes[j])
+					copy(h, v[:])
+				} else {
+					break
+				}
 			} else {
 				// keep merging up if we are the right side
 				v := hasher.Combi(tmp[j], hArr)
@@ -76,33 +87,24 @@ func Merkleize(hasher HashFn, count uint32, leaf func(i uint32) []byte) (out [32
 		// store the merge result (may be no merge, i.e. bottom leaf node)
 		copy(tmp[j][:], h)
 	}
-	// if count is not a power of 2
-	if (count-1)&count != 0 {
-		i := count
-		j = 0
-		// walk up to the first right side
-		for ; j <= depth; j++ {
-			if i&(uint32(1)<<j) != 0 {
-				break
-			}
-		}
-		// the initial merge in is mixing in work from the right.
-		// Initial work is the zero-hash at height j
-		copy(h, ZeroHashes[j][:])
-		for ; j < depth; j++ {
-			if i&(uint32(1)<<j) == 0 {
-				// left side: combine previous with zero-hash
-				// i.e. venture out to merge back closer to the root
-				v := hasher.Combi(hArr, ZeroHashes[j])
-				copy(h, v[:])
-			} else {
-				// right side: combine left side with previous
-				// i.e. merge back with the work
-				v := hasher.Combi(tmp[j], hArr)
-				copy(h, v[:])
-			}
-		}
-		return hArr
+
+	// merge in leaf by leaf.
+	for i := uint32(0); i < count; i++ {
+		copy(h[:], leaf(i))
+		merge(i)
 	}
-	return tmp[j]
+
+	// complement with 0 if empty, or if not the right power of 2
+	if (uint32(1) << depth) != count {
+		copy(h[:], ZeroHashes[0][:])
+		merge(count)
+	}
+
+	// the next power of two may be smaller than the ultimate virtual size,
+	// complement with zero-hashes at each depth.
+	for j := depth; j < limitDepth; j++ {
+		tmp[j + 1] = hasher.Combi(tmp[j], ZeroHashes[j])
+	}
+
+	return tmp[limitDepth]
 }
